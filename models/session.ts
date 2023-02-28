fd9565856a8ff3ad00bd85ab0ed31ec359807319
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-02-28 15:21:06 +0530

    Updated the sessions model

---
+++ models/session.ts
@@ -0,0 +1,17 @@
+import { model, Schema } from "mongoose"
+import Session from "../types/session"
+
+export default model<Session>("session", new Schema({
+    token: {
+        type: String,
+        required: [true, "The token of the session is required"]
+    },
+    timestamp: {
+        type: String,
+        default: new Date(Date.now()).toUTCString()
+    },
+    email: {
+        type: String,
+        required: [true, "The email of the user session is required"]
+    }
+}))
