683b5b30dcdcb2698e21965be341b78c33ecb9b0
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-03-04 07:04:59 +0530

    Updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,30 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  setTimeout(() => setIsLoading(false), 3000)
+  useEffect(() => {
+    const element = document.getElementsByClassName("App")[0]
+    const view = document.defaultView
+    view.scrollTo({
+      left: (element.clientWidth-view.innerWidth)/2,
+      top: (element.clientHeight-view.innerHeight)/2
+    })
+  }, [])
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="/test">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div 
+      className="App"
+      style={{
+        width: 4000,
+        height: 2000,
+        backgroundImage: "linear-gradient(red, yellow)",
+        display: "flex",
+        justifyContent: "center",
+        "alignItems": "center"
+      }}
+    >
+      <h1>Something</h1>
     </div>
   )
 }
