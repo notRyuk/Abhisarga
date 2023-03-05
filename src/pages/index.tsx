bf20dfbae4392b525216d2e39043ac1c24eb7ccc
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-03-05 09:50:06 +0530

    Updated landing page background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, MouseEvent } from 'react'
 import backgroundImage from "../assets/background.png";
 import Logo from '../components/Logo';
 import Modal from "../components/CustomCard"
@@ -39,6 +39,35 @@ function App() {
       left: (element.clientWidth-view.innerWidth)/2,
       top: (element.clientHeight-view.innerHeight)/2
     })
+
+    var _startX = 0;
+    var _startY = 0;
+    var _offsetX = 0;           
+    var _offsetY = 0;
+    var _dragElement;
+    document.onmousedown = OnMouseDown;
+    document.onmouseup = OnMouseUp;
+
+    function OnMouseDown(event: any){
+      console.log(event)
+      document.onmousemove = OnMouseMove;
+      _startX = event.clientX;
+      _startY = event.clientY;
+      _offsetX = document.getElementById('div1').offsetLeft;
+      _offsetY = document.getElementById('div1').offsetTop;
+      _dragElement = document.getElementById('div1')
+
+    }
+
+    function OnMouseMove(event: any){
+        _dragElement.style.left = (_offsetX + event.clientX - _startX) + 'px';
+      _dragElement.style.top = (_offsetY + event.clientY - _startY) + 'px';
+    }
+
+    function OnMouseUp(event: any){
+      document.onmousemove = null;
+      _dragElement=null;
+    }
   }, [])
   
   return (
